093d4b0ade5a1be4506b2807b1874f5f2b6a6a36
Mandy McClausky <me@mandynicole.com>
2015-11-20 10:37:14 -0800

    Clear git cache

--- app/routes/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'The ACT-W Conference Home Page' });
-});
-
-module.exports = router;
