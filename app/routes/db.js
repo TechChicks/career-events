ee3e5879a85af8fdb3e8672bde2478240979bbe8
Shaelyn <shaelynjoy@gmail.com>
2016-02-02 20:00:31 -0800

    auth attempt. not working

--- app/routes/db.js
+++ app/routes/db.js
@@ -1,5 +1,55 @@
+var Sequelize = require('sequelize');
+// var pg = require('pg').native;
 var pg = require('pg');
+var passport = require("passport");
+var PassportLocalStrategy = require('passport-local').Strategy;
 var connectionString = require('../config.json');
+
+// var sequelize = new Sequelize('actw_development', 'swatson', {
+//   host: 'localhost',
+//   dialect: 'postgres'
+// });
+
+var sequelize = new Sequelize(connectionString.postgres);
+
+var User = sequelize.define('user', {
+  username: Sequelize.STRING,
+  password: Sequelize.STRING,
+});
+
+User.sync();
+
+var auth = {};
+auth.localStrategy = new PassportLocalStrategy({
+	username: 'username',
+	password: 'password'
+  },
+
+  function (username, password, done){
+    var User = require('./users');
+    User.find({username: username}).success(function(user){
+      if (!user){
+        return done(null, false, { message: 'Nobody here by that name'} );
+      }
+      if (user.password !== password){
+        return done(null, false, { message: 'Wrong password'} );
+      }
+      return done(null, { username: user.username });
+    });
+  }
+);
+
+auth.validPassword = function(password){
+  return this.password === password;
+}
+
+auth.serializeUser = function(user, done){
+  done(null, user);
+};
+
+auth.deserializeUser = function(obj, done){
+  done(null, obj);
+};
 
 module.exports = {
   query: function(text, values, cb) {
