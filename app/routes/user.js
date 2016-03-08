6104325131aee2e9e957bc1e420d0667587a4df3
Shaelyn <shaelynjoy@gmail.com>
2016-03-07 19:22:18 -0800

    print user

--- app/routes/user.js
+++ app/routes/user.js
@@ -5,7 +5,7 @@ exports.signUp = function(req, res) {
 }
 
 exports.register = function(req, res){
-	console.log('!!!REGISTER', req.body);
+	console.log('!!!REGISTER', req.body, db.User);
 	 db.User.find({where: {username: req.username}}).then(function (user){
 		if(!user) {
 			db.User.create({username: req.body.username, password: req.body.password}).error(function(err){
